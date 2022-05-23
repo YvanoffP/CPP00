e89d840093e6a78eba648a6ad58ae81fd20d6cc4
YvanoffP <petruzzi.yvan@gmail.com>
2022-05-23 15:03:46 +0200

    maybe will do it later

---
+++ ex02/Account.cpp
@@ -0,0 +1,49 @@
+#include "Account.hpp"
+
+Account::Account( int initial_deposit ) {
+    this->_amount = initial_deposit;
+}
+
+Account::~Account( void ) {
+
+}
+
+Account::getNbAccounts( void ) {
+
+}
+
+Account::getTotalAmount( void ) {
+
+}
+
+Account::getNbDeposits( void ) {
+
+}
+
+Account::getNbWithdrawals( void ) {
+
+}
+
+Account::displayAccountsInfos( void ) {
+
+}
+
+Account::makeDeposit( void ) {
+
+}
+
+Account::makeWithdrawal( void ) {
+
+}
+
+Account::checkAmount( void ) {
+
+}
+
+Account::displayStatus( void ) {
+
+}
+
+Account::_displatTimestamp( void ) {
+
+}
